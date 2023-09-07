b01b443e3bd38de21cb1abf2c5775250e5c13fd1
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2023-09-06 19:19:38 -0700

    Add files via upload

--- parcial1/practica3.js
+++ parcial1/practica3.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
-    var container = 0;
-    $('img').click(function(){
-        var nombre_imagen = $(this).attr('data-id');
-        $(this).sttr('src', imagenes + '/' + nombre_imagen);
-    })
+    var par = false;
+    var carta1 = ""; var carta2 = "";
+    var total_pares = 0;
 
+    $('img').click(function(e){
+        var estado = $(this).attr('data-estado');
+        var nombre_imagen = $(this).attr('data-id');
+
+        if(estado == "0"){
+            if(carta1 == ""){
+                carta1 = $(this);
+                $(this).attr('src', nombre_imagen);
+                console.log("se asign carta #1");
+            } else{
+                carta2 = $(this);
+                carta2.attr('src', nombre_imagen);
+                console.log("Se asigno carta #2");
+            }
+        }
+        if(carta1 != "" && carta2 != ""){
+            if(carta1.attr('data-id') == carta)
+        }
+
+    });
 })
