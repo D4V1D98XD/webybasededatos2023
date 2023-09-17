4d2843ce5cea62e31c737a6cae2e204a9c5b5c66
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2023-09-17 11:37:06 -0700

    Por fin funciona el hdsptm

--- parcial1/PROYECTO_Juego_Culebra/js/script.js
+++ parcial1/PROYECTO_Juego_Culebra/js/script.js
@@ -8,14 +8,9 @@ $(document).ready(function () {
     var posX = 250;
     var posY = 150;
 
-    // Variables para la posición de la comida
-    var comidaX = 0;
-    var comidaY = 0;
-
     // Tamaño inicial de la serpiente
     var snakeSize = 1;
     var partesCuerpo = [];
-
 
     // Función para iniciar el juego
     function iniciarJuego() {
@@ -31,62 +26,24 @@ $(document).ready(function () {
 
             // Iniciar el bucle de juego para mover la serpiente continuamente
             intervaloMovimiento = setInterval(moverSerpiente, velocidad);
-
-            console.log("hola?");
         }
     }
 
     function mostrarComidaAleatoria() {
         var maxX = 480; // Ancho del área del juego
         var maxY = 280; // Altura del área del juego
-        comidaX = Math.floor(Math.random() * maxX / 20) * 20; // Posición X aleatoria en múltiplos de 20
-        comidaY = Math.floor(Math.random() * maxY / 20) * 20; // Posición Y aleatoria en múltiplos de 20
+        var comidaX = Math.floor(Math.random() * maxX / 20) * 20; // Posición X aleatoria en múltiplos de 20
+        var comidaY = Math.floor(Math.random() * maxY / 20) * 20; // Posición Y aleatoria en múltiplos de 20
     
-        // Crea un nuevo elemento de comida y establece su posición
-        var nuevaComida = $("<div class='food'></div>");
-        nuevaComida.css({ left: comidaX + "px", top: comidaY + "px" });
-    
-        // Agrega la nueva comida al DOM
-        $(".game-container").append(nuevaComida);
-    
-        // Muestra la nueva comida
-        nuevaComida.css("display", "block");
+        // Actualizar la posición de la comida en el DOM
+        $(".food").css({ left: comidaX + "px", top: comidaY + "px" });
+
+        // Mostrar la comida
+        $(".food").css("display", "block");
     }
-
-    function agregarParteCuerpo(x, y) {
-        // Crea un nuevo elemento para la parte del cuerpo
-        var nuevaParteCuerpo = $("<div class='snake snake-body'></div>");
-
-        // Establece la posición de la nueva parte del cuerpo
-        nuevaParteCuerpo.css({ left: x + "px", top: y + "px" });
-
-        // Agrega la nueva parte del cuerpo al DOM
-        $(".game-container").append(nuevaParteCuerpo);
-
-        // Muestra la nueva parte del cuerpo
-        nuevaParteCuerpo.css("display", "block");
-
-        // Agrega la nueva parte del cuerpo al arreglo
-        partesCuerpo.push(nuevaParteCuerpo);
-    }
-
-    function moverPartesCuerpo() {
-        // Mueve cada parte del cuerpo hacia la posición de la parte delante de ella
-        for (var i = partesCuerpo.length - 1; i > 0; i--) {
-            var parteActual = partesCuerpo[i];
-            var parteAnterior = partesCuerpo[i - 1];
-            var left = parteAnterior.css("left");
-            var top = parteAnterior.css("top");
-            parteActual.css({ left: left, top: top });
-        }
-    }
-
 
     // Función para mover la serpiente
     function moverSerpiente() {
-        // Mover las partes del cuerpo de la serpiente primero
-        moverPartesCuerpo();
-    
         // Mueve la serpiente en la dirección actual
         if (direccionActual === "derecha") {
             posX += 20; // Mover hacia la derecha
@@ -97,31 +54,50 @@ $(document).ready(function () {
         } else if (direccionActual === "abajo") {
             posY += 20; // Mover hacia abajo
         }
-    
+
         // Actualizar la posición de la cabeza de la serpiente en el DOM
         $(".snake").css({ left: posX + "px", top: posY + "px" });
-    
+
+        // Agregar una nueva parte al cuerpo de la serpiente
+        partesCuerpo.push({ x: posX, y: posY });
+
+        // Eliminar la última parte del cuerpo si la serpiente no ha crecido
+        if (partesCuerpo.length > snakeSize) {
+            partesCuerpo.shift();
+        }
+
+        // Actualizar la posición de las partes del cuerpo
+        moverPartesCuerpo();
+
         // Comprobar colisión con la comida
         verificarColision();
     }
 
     // Comprobar colisión entre la cabeza de la serpiente y la comida
     function verificarColision() {
-        console.log("verificacion si funciona");
-        console.log("posicion serpiente", posX);
-        console.log("posicion comida", comidaX);
-        if (comidaX + 10 == posX && comidaY + 10 == posY) {
-            console.log("Colisión detectada");
-            $(".food").remove();
-            $("#Puntos").text(snakeSize * 100);
-            snakeSize++;
-            agregarParteCuerpo(posX - 20, posY); 
-            // Mostrar una nueva comida en una ubicación aleatoria
+        var comidaX = parseInt($(".food").css("left"));
+        var comidaY = parseInt($(".food").css("top"));
+        
+        if (posX == comidaX + 10 && posY == comidaY + 10) {
+            // La serpiente ha tocado la comida
+            snakeSize++; // Aumentar el tamaño de la serpiente
+            $("#Puntos").text(snakeSize * 100); // Actualizar los puntos
+
+            // Generar una nueva posición aleatoria para la comida y mostrarla
             mostrarComidaAleatoria();
+
+            // Agregar una nueva parte al cuerpo de la serpiente
+            partesCuerpo.push({ x: posX, y: posY });
         }
     }
 
-
+    // Función para mover las partes del cuerpo de la serpiente
+    function moverPartesCuerpo() {
+        for (var i = 0; i < partesCuerpo.length; i++) {
+            var parte = partesCuerpo[i];
+            $(".snake-body:eq(" + i + ")").css({ left: parte.x + "px", top: parte.y + "px" });
+        }
+    }
 
     // Detectar la pulsación de una tecla para cambiar la dirección de la serpiente
     $(document).keydown(function (event) {
@@ -139,6 +115,5 @@ $(document).ready(function () {
         } else if (event.key === "ArrowDown" && direccionActual !== "arriba") {
             direccionActual = "abajo";
         }
-        
     });
 });
