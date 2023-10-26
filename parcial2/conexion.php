f74834d75601eb6822f60fa85fab78b48bd2a0ec
D4V1D98XD <124410459+D4V1D98XD@users.noreply.github.com>
2023-10-25 19:32:36 -0700

    Add files via upload

--- parcial2/conexion.php
+++ parcial2/conexion.php
@@ -1,11 +1,16 @@
 <?php
+
 $server = "localhost";
 $user = "root";
-$password = "";
+$password = ""; //es el password del CHAMP
+$base_de_datos = "cetis107";
 
-$conexion = new mysqli($server, $user, $password);
+//conexion
+$base_de_datos = "cetis107";
+$conexion = new mysqli($server, $user, $password, $base_de_datos); 
 
+//revisar conexion
 if($conexion->connect_error){
-    die("Fallo la conexion" .$conexion->connect_error);
+    die("falló la conexión" . $conexion->connect_error);
 }
 ?>
--- parcial2/menu.php
+++ parcial2/menu.php
@@ -1,6 +1,7 @@
- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- copia el nav del bustra -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">CETIS 107</a>
+    <a class="navbar-brand" href="#">TuMamáyYoHacemosComplot</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,22 +15,22 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Menú
+            Lista Desplegable
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Inicio</a></li>
-            <li><a class="dropdown-item" href="#">Registra alumnos</a></li>
+            <li><a class="dropdown-item" href="#">Acción</a></li>
+            <li><a class="dropdown-item" href="consultarAlumnos.php">Consultar alumnos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Consulta alumnos</a></li>
+            <li><a class="dropdown-item" href="#">Regristrar Alumno</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Desabilitar</a>
         </li>
       </ul>
       <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input class="form-control me-2" type="Buscar" placeholder="Buscar" aria-label="Buscar">
+        <button class="btn btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
